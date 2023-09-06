ec4799dc1d7d38704fb195e779055f055667d5bf
Oliver Anglin <88408598+oliveranglin@users.noreply.github.com>
2023-09-06 18:14:47 +0100

    Updated Look and feel... still need to finish contact page

--- JS/fadeIn.js
+++ JS/fadeIn.js
@@ -1,24 +1,29 @@
 
-  $(document).ready(function() {
+$(document).ready(function() {
       
-      /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
+/* Every time the window is scrolled ... */
+$(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.hideme').each( function(i){
+        
+        var middle_of_object = $(this).position().top + $(this).outerHeight()/10;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is 1/4 visible in the window, fade it in */
+        if( bottom_of_window > middle_of_object ){
+            
+            $(this).animate({'opacity':'1'},1000);
+            $(this).css({
+                'transform': 'rotateY(0deg) rotateX(0deg)',
+                'transition': 'transform 1s ease-in-out',
+                'animation-fill': 'forwards'
+              });
+                
+        }
+        
+    }); 
+
+});
       
-          /* Check the location of each desired element */
-          $('.hideme').each( function(i){
-              
-              var middle_of_object = $(this).position().top + $(this).outerHeight()/4;
-              var bottom_of_window = $(window).scrollTop() + $(window).height();
-              
-              /* If the object is 1/4 visible in the window, fade it in */
-              if( bottom_of_window > middle_of_object ){
-                  
-                  $(this).animate({'opacity':'1'},1000);
-                      
-              }
-              
-          }); 
-      
-      });
-      
-  });
+});
